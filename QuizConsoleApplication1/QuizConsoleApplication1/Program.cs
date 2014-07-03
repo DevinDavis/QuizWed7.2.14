1b5af77741dab09c6ed07fec3da9896188b79b3d
Devin Davis <dndavis1111@gmail.com>
2014-07-02 20:45:10 -0700

    Find Smallest Integer

---
+++ QuizConsoleApplication1/QuizConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizConsoleApplication1
+{
+    public class FindSmallest
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 50;
+            int ret;
+            FindSmallest n = new FindSmallest();
+
+            ret = n.FindMin(a, b);
+            Console.WriteLine("Smallest Interger is {0}", ret);
+            Console.ReadLine();
+        }
+        public int FindMin(int a, int b)
+        {
+            int result;
+
+            if (a > b)
+                result = a;
+
+            else
+                result = b;
+
+            return result;
+        }
+    }
+}
+
